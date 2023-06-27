123f3650b97bde1502e295953963cc3189f0fb90
stevieg-o1 <steven.karanja@student.moringaschool.com>
2023-06-27 13:43:53 +0300

    completed assignment

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,8 @@ ActiveRecord::Schema.define(version: 2021_05_02_132944) do
 
   create_table "birds", force: :cascade do |t|
     t.string "name"
+
+    
     t.string "species"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
